3da50d8e5c9323f147525c0ab2429d6a35c6e06c
Billy Otieno Okendo <billyotieno7@gmail.com>
2022-09-09 01:27:31 +0300

    modified index.css

--- house/src/components/Home.js
+++ house/src/components/Home.js
@@ -3,6 +3,7 @@ import React from 'react'
 function Home() {
   return (
     <div className='home'>
+      <h1>HOUSE MANAGEMENT</h1>
       <marquee direction="DOWN">
         <p>WELCOME</p>
       </marquee>
